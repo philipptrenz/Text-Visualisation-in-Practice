b9dbe6352dba3f48c82209c704a0d7a3d8c4b609
philipptrenz <mail@philipptrenz.de>
2019-05-18 12:23:08 +0200

    Draft of blogpost 06

--- 06_text_graphs/06_text_graphs.py
+++ 06_text_graphs/06_text_graphs.py
@@ -11,6 +11,121 @@ from gensim.corpora import Dictionary
 from sklearn.metrics.pairwise import cosine_similarity
 
 
+def calculate_nodes(dct, df):
+
+    print('preparing node export')
+    all_nodes = []
+
+    for token_id in dct:
+        token = dct[token_id]
+        count = 0
+
+        for index, row in df.iterrows():
+            if token in row['article']: count += 1
+
+        #if count > 10:  # min count
+        all_nodes.append({
+            'single_couses': 0,
+            'name': token,
+            'uses': count
+        })
+
+        print('processed', len(all_nodes), 'of', len(dct), 'nodes')
+
+    return all_nodes
+
+
+def get_all_nodes(dct, df):
+    all_nodes = []
+    all_nodes_file = 'nodes_all.json'
+
+    if not os.path.isfile(all_nodes_file):
+        with open(all_nodes_file, 'w') as f:
+
+            # calculate all nodes
+            all_nodes = calculate_nodes(dct, df)
+
+            f.write(json.dumps(all_nodes))
+    else:
+        with open(all_nodes_file) as f:
+            all_nodes = json.load(f)
+
+    return all_nodes
+
+
+def calculate_edges(export_nodes, model):
+    all_edges = []
+    print('calculating edges ...')
+    i = 0
+    for n1 in export_nodes:
+
+        for n2 in export_nodes:
+            token1 = n1["name"]
+            token2 = n2["name"]
+            if token1 == token2: continue
+
+            sim = cosine_similarity(model[token1].reshape(1, -1), model[token2].reshape(1, -1))[0,0]
+            all_edges.append({
+                'source': token1,
+                'target': token2,
+                'value': float(sim)
+            })
+
+        i += 1
+        print('processed', i, 'of', len(all_edges), 'edges')
+
+    return all_edges
+
+def get_all_edges(export_nodes, model):
+    all_edges = []
+    all_edges_file = 'links_all.json'
+    if not os.path.isfile(all_edges_file):
+        with open(all_edges_file, 'w') as f:
+
+            # calculate all edges
+            all_edges = calculate_edges(export_nodes, model)
+
+            f.write(json.dumps(all_edges))
+    else:
+        with open(all_edges_file) as f:
+            all_edges = json.load(f)
+
+    return all_edges
+
+
+
+def get_fasttext_model(df):
+
+    fasttext_model_file = 'fasttext.model'
+    model = None
+    if not os.path.isfile(fasttext_model_file):
+        print('building fasttext model ...')
+
+        size = 50
+        window = 5
+        sentences = df['article']
+        total_examples = len(sentences)
+        epochs = 10
+
+        model = FastText(size=size, window=window)  # instantiate
+        model.build_vocab(sentences=sentences)
+        model.train(sentences=sentences, total_examples=total_examples, epochs=epochs)  # train
+        model.save(fasttext_model_file)
+    else:
+        print('loading fasttext model ...')
+        model = FastText.load(fasttext_model_file)
+
+    return model
+
+def write_export(export_nodes, export_edges):
+
+    with open('nodes.json', 'w') as f:
+        f.write(json.dumps(export_nodes))
+
+    with open('links.json', 'w') as f:
+        f.write(json.dumps(export_edges))
+
+
 if __name__ == '__main__':
 
     from data_loader import DataLoader
@@ -32,103 +147,52 @@ if __name__ == '__main__':
 
     corpus_tfidf = model[corpus]
 
-    #########
 
-    def calculate_nodes():
+    ####################################
 
-        print('preparing node export')
-        export_nodes = []
 
-        for token_id in dct:
-            token = dct[token_id]
-            count = 0
+    print('getting all nodes ...')
+    all_nodes = get_all_nodes(dct, df)
 
-            for index, row in df.iterrows():
-                if token in row['article']: count += 1
 
-            #if count > 10:  # min count
-            export_nodes.append({
-                'single_couses': 0,
-                'name': token,
-                'uses': count
-            })
-
-            print(token_id, '/', len(dct))
-
-        print(f'exporting {len(export_nodes)} nodes')
-
-        print('writing node exports')
-        with open('nodes_all.json', 'w') as f:
-            f.write(json.dumps(export_nodes))
-    # calculate_nodes()
-
+    print('filtering all nodes to relevant ones ...')
     export_nodes = []
-    with open('nodes_all.json') as json_file:
-        all_nodes = json.load(json_file)
-
-        for n in all_nodes:
-            if n["uses"] > 25: # if word occures in more than 15 documents
-                export_nodes.append(n)
+    for n in all_nodes:
+        # TODO: Adjust value
+        if n["uses"] > 40: # if word occures in more than X documents
+            export_nodes.append(n)
 
 
-    PATH_fasttext = 'fasttext.model'
-    model = None
-    if not os.path.isfile(PATH_fasttext):
-        print('building fasttext model ...')
+    print('getting fasttext model ...')
+    model = get_fasttext_model(df)
 
-        size = 200
-        window = 5
-        sentences = df['article']
-        total_examples = len(sentences)
-        epochs = 10
 
-        model = FastText(size=size, window=window)  # instantiate
-        model.build_vocab(sentences=sentences)
-        model.train(sentences=sentences, total_examples=total_examples, epochs=epochs)  # train
-        model.save(PATH_fasttext)
-    else:
+    print('getting all edges ...')
+    all_edges = get_all_edges(export_nodes, model)
 
-        print('loading fasttext model ...')
-        model = FastText.load(PATH_fasttext)
 
-    vocab = list(model.wv.vocab)
+    print('filtering all edges to relevant ones ...')
+    '''
+    export_edges = []
+    for l in all_edges:
+        # TODO: Adjust value
+        if l['value'] > 0.9:
+            export_edges.append(l)
+    '''
+    # get the 1000 most significant edges
+    export_edges=sorted(all_edges, key=lambda k:k['value'])[-1000:]
+    print('export_edges', export_edges)
 
-    print('calculating edges ...')
-    similarities = []
-    export_links = []
 
-    i=0
-    for n1 in export_nodes:
-        for n2 in export_nodes:
-        #if source in relevant_token and target in relevant_token:
-            token1 = n1["name"]
-            token2 = n2["name"]
-
-            if token1 == token2: continue
-
-            sim = cosine_similarity(model[token1].reshape(1, -1), model[token2].reshape(1, -1))[0,0]
-
-            if sim > 0.95:
-                export_links.append({
-                    'source': token1,
-                    'target': token2,
-                    'value': float(sim)
-                })
-
-        #if len(export_links) > 2000 and not False: break
-        i += 1
-        print(i, '/', len(export_nodes), '(', len(export_links), 'elements)')
-    print(f'exporting {len(export_links)} links')
+    ####################################
 
     print('calculating couses (whatever that is)')
     for node in export_nodes:
-        for link in export_links:
-            if link['source'] == node['name'] or link['target'] == node['name']:
-                node['single_couses'] += link['value']
+        for edge in export_edges:
+            if edge['source'] == node['name'] or edge['target'] == node['name']:
+                node['single_couses'] += edge['value']
+
+    ####################################
 
     print('writing exports')
-    with open('links.json', 'w') as f:
-        f.write(json.dumps(export_links))
-
-    with open('nodes.json', 'w') as f:
-        f.write(json.dumps(export_nodes))
+    write_export(export_nodes, export_edges)
